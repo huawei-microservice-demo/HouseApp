91a43118cb6de933dd0c953335e714bbab440f0f
liubao <bao.liu@huawei.com>
2019-01-08 16:27:30 +0800

    HouseAPP改造为最新版本：去掉2PC演示；WEB改为jar包启动；修改配置

---
+++ customer-website/src/main/java/com/huawei/cse/houseapp/website/WebsiteApplication.java
@@ -0,0 +1,17 @@
+package com.huawei.cse.houseapp.website;
+
+import org.apache.servicecomb.springboot2.starter.EnableServiceComb;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableServiceComb
+public class WebsiteApplication {
+  public static void main(String[] args) throws Exception {
+    try {
+      SpringApplication.run(WebsiteApplication.class, args);
+    } catch (Throwable e) {
+      e.printStackTrace();
+    }
+  }
+}
